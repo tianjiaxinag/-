c0ca919121b984881ac0e3475c80745fcbe43bb2
tianjiaxinag <1561711487@qq.com>
2021-03-31 16:42:14 +0800

    维护一下

--- sql.js
+++ sql.js
@@ -9,17 +9,17 @@ const cors = require('cors');
 server.use(cors());
 
 // 验证token
-// const jwt = require('express-jwt');
-// app.use(jwt().unless());
+const jwt = require('express-jwt');
+app.use(jwt().unless());
 // jwt() 用于解析token，并将 token 中保存的数据 赋值给 req.user
 // unless() 约定某个接口不需要身份认证
-// server.use(jwt({
-//   secret: 'gz61', // 生成token时的 钥匙，必须统一
-//   algorithms: ['HS256'] // 必填，加密算法，无需了解
-// }).unless({
-//   path: ['/api/login','/api/register', /^\/uploads\/.*/] // 除了这两个接口，其他都需要认证
-// })
-// )
+server.use(jwt({
+  secret: 'gz61', // 生成token时的 钥匙，必须统一
+  algorithms: ['HS256'] // 必填，加密算法，无需了解
+}).unless({
+  path: ['/api/login','/api/register', /^\/uploads\/.*/] // 除了这两个接口，其他都需要认证
+})
+)
 // 路由中间件
 const userRouter = require('./router/user-router.js');
 const accountRouter =require('./router/accout_router.js');
